9396c5d16050bda2e6c0da292d5cb26b19a557dc
dh-you <dh-you@YDH>
2019-11-20 10:43:25 +0900

    코드 정리 + ejs + script태그 사용

--- server.js
+++ server.js
@@ -5,23 +5,23 @@ server.set('view engine', 'ejs'); // view engine ejs 사용하기
 server.set('views', './');  // views 경로 바꾸기
 
 server.get('/', function (req, res) {
-    res.render('./view.ejs', {type : 'all', data : 'data', p : ''}); // ejs 렌더링
+    res.render('./view.ejs', {type : 'all', data : ['hammer', 'warrior'], p : ''}); // ejs 렌더링
 });
 
 server.get('/items/', function (req, res) {
-    res.render('./view.ejs', {type : 'items',  data : 'data', p : ''}); // ejs 렌더링
+    res.render('./view.ejs', {type : 'items',  data : 'hammer', p : ''}); // ejs 렌더링
 });
 
 server.get('/items/:itemNo', function (req, res) {
-    res.render('./view.ejs', {type : 'items',  data : 'data', p : req.params.itemNo }); // ejs 렌더링
+    res.render('./view.ejs', {type : 'items',  data : 'hammer', p : req.params.itemNo }); // ejs 렌더링
 });
 
 server.get('/users', function (req, res) {
-    res.render('./view.ejs', {type : 'users', data : 'data', p : ''}); // ejs 렌더링
+    res.render('./view.ejs', {type : 'users', data : 'warrior', p : ''}); // ejs 렌더링
 });
 
 server.get('/users/:userNo', function (req, res) {
-    res.render('./view.ejs', {type : 'users',  data : 'data', p : req.params.userNo }); // ejs 렌더링
+    res.render('./view.ejs', {type : 'users',  data : 'warrior', p : req.params.userNo }); // ejs 렌더링
 });
 
 //var ip = '127.0.0.1';
